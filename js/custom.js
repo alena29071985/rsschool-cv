b2da47b4a843b3cacbb9c80af981c3528db8d26f
Alena Kvitinskaya <alena.kvitinskaya@gmail.com>
2020-09-20 13:57:12 +0300

    my cv in html

---
+++ js/custom.js
@@ -0,0 +1,54 @@
+/*=========================================================================
+        Preloader
+=========================================================================*/
+$(window).load(function() {
+    "use strict";
+    $(".preloader-outer").delay(350).fadeOut('slow');
+});
+
+$(function(){
+    "use strict";
+    /*=========================================================================
+            One Page Nav
+    =========================================================================*/
+    $(".navigation").onePageNav({
+        currentClass: 'current',
+        changeHash: false,
+        scrollSpeed: 700,
+        scrollThreshold: 0.5,
+        easing: 'easeInOutCubic'
+    });
+
+    /*=========================================================================
+            Portfolio filter
+    =========================================================================*/
+      if($('#works .item-outer').length > 0){
+        var filterizd = $('#works .item-outer').filterizr();
+      }
+      $( '.control ul li' ).on( 'click', function() {
+            $( this ).parent().find( 'li.active' ).removeClass( 'active' );
+            $( this ).addClass( 'active' );
+      });
+
+    /*=========================================================================
+            Hamburger Menu & Mobile Push menu
+    =========================================================================*/
+    $(".hamburger-menu, .main-nav ul li a").on( 'click', function() {
+        $(".header").toggleClass("pushed");
+        $(".main-content").toggleClass("main-pushed");
+        $('.bar').toggleClass('animate');
+    });
+
+    /*=========================================================================
+            Welcome & Header Height
+    =========================================================================*/
+    $("#welcome").css({'height':($(window).height())+'px'});
+    $(".header").css({'height':($(window).height())+'px'});
+
+    /*=========================================================================
+            Magnific Popup Functions
+    =========================================================================*/
+    $('.work-image').magnificPopup({
+      type: 'image'
+    });
+});
